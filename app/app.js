215c1f64d67c2f3932df34458903516d9ffa1d70
jamiromic <jamiromic@gmail.com>
2022-07-19 16:47:23 +0200

    exercise_done

--- app/app.js
+++ app/app.js
@@ -2,18 +2,33 @@
 
 
 // Dichiarazione + Assegnazione nome Variabile inline
+// Con il comando prompt interagisco con l'utente tramite browser
 
-let name = prompt('Mi daresti il tuo nome?')
-let surname = prompt('Ora il tuo cognome?')
-let color = prompt('Il tuo colore preferito?')
+let myName = prompt('Mi daresti il tuo nome?');
+let mySurname = prompt('Ora il tuo cognome?');
+let myColor = prompt('Il tuo colore preferito?');
 
 // Assegnazione nome Variabile inline
 
-const number = 21;
+// Con questo comando inserisce un numero Random
+const myNumber = Math.random( );
 
-console.log('name')
+console.log (myName + mySurname + myColor)
 
-// Con il comando prompt interagisco con l'utente tramite browser
+
+// Recupero elemento dal codice HTML in cui andremo a stampare il messaggio
+const password = document.getElementById('password')
+
+console.dir(password)
+// Stampa del contenuto
+
+password.innerHTML = myName + mySurname + myColor + myNumber
+
+
+
+
+
+
 
 
 
