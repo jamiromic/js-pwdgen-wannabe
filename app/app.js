ba4a4e65ffbd86bfe1d75b84eb75bad60ce18505
jamiromic <jamiromic@gmail.com>
2022-07-19 15:48:27 +0200

    add structure

---
+++ app/app.js
@@ -0,0 +1,19 @@
+// Primo File Javascript
+
+
+// Dichiarazione + Assegnazione nome Variabile inline
+
+let name = prompt('Mi daresti il tuo nome?')
+let surname = prompt('Ora il tuo cognome?')
+let color = prompt('Il tuo colore preferito?')
+
+// Assegnazione nome Variabile inline
+
+const number = 21;
+
+console.log('name')
+
+// Con il comando prompt interagisco con l'utente tramite browser
+
+
+
